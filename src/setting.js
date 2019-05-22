21dcca09255bf1a8e6d7f25d1aa3fbeaaa9a5bf8
Trang <hoangthiminhtrang304@gmail.com>
2019-05-22 15:39:33 +0700

    setting the fundamental functions of to do list

--- src/setting.js
+++ src/setting.js
@@ -2,6 +2,8 @@ import {getData, setLocalStorage} from './store.js';
 import {displayProject, displayInfo} from './display.js';
 
 const reset = (projects, change,e) =>{
+    console.table(projects);
+    setLocalStorage(projects);
     displayProject(projects);
     if(change){
         displayInfo(e);
@@ -9,30 +11,31 @@ const reset = (projects, change,e) =>{
         e.target.parentNode.innerHTML = '';
     }
 }
-
 export const deleteProject = (e) =>{
     const projects = getData();
     const index = Number(e.target.getAttribute('data-key'));
     projects.splice(index,1);
-    console.log(projects);
-    setLocalStorage(projects);
     reset(projects, false, e);
-    
 }
 const getTask = (e) =>{
     const projects = getData();
-    const proIndex = e.target.getAttribute('data-key');
-    const taskIndex = e.target.getAttribute('data-index');
-    return {projects, proIndex, taskIndex}
+    const index = Number(e.target.parentNode.parentNode.getAttribute('data-key'));
+    const project = projects[index]
+    const indexTask = Number(e.target.parentNode.getAttribute('data-task'));
+    return {projects,project, indexTask}
 }
-const deleteTask = (tasks, index) =>{  
-    tasks.splice(index, 1);
+export const deleteTask = (e) =>{  
+    const info = getTask(e);
+    const tasks = info.project.tasks;
+    tasks.splice(info.indexTask, 1);
+    reset(info.projects,true, e);
+    
 }
 const changeState = (task) =>{
     let state = task.state;
     if(state == 'uncomplete'){
         return 'complete';
-    }else 'complete';
+    }else  return 'uncomplete';
 }
 const changePriority = (task) =>{
     let priority = task.priority;
@@ -42,3 +45,18 @@ const changePriority = (task) =>{
         return 'high';
     }else return 'low';
 }
+const changeTaskState = (e) =>{
+    const info = getTask(e);
+    const indexTask = info.indexTask;
+    const task = info.project.tasks[indexTask];
+    task.state = changeState(task);
+    reset(info.projects,true, e);
+}
+const changeTaskPrior = (e) =>{
+    const info = getTask(e);
+    const indexTask = info.indexTask;
+    const task = info.project.tasks[indexTask];
+    task.priority = changePriority(task);
+    reset(info.projects,true, e);
+}
+export {changeTaskPrior, changeTaskState}
--- src/taskDisplay.js
+++ src/taskDisplay.js
@@ -1,27 +1,28 @@
+import {deleteTask, changeTaskPrior, changeTaskState} from './setting.js';
+
 const createTaskContainer = () =>{
     const child = document.createElement('div');
     child.className = 'info';
     document.querySelector('.projectDisplay').appendChild(child);
 }
-const taskTitle = (title, index) =>{
+const taskTitle = (title, index, indexTask) =>{
     console.log(title);
     let child = document.createElement('div');
     child.innerHTML = `<h3>${title}</h3>`;
     child.className = 'tasktitle';
-    child.setAttribute('data-key', index);
+    addButton(child, index, indexTask);
     const parent = document.querySelector('.info');
     parent.appendChild(child);
-
 }
-const taskDetail = (task, index) =>{
+const taskDetail = (task, index, indexTask) =>{
     const parent = document.querySelector('.info');
     const child = document.createElement('div');
     child.className = 'taskDetail';
     child.setAttribute('data-key', index);
     taskInfo(child, 'Description', task.description);
     taskInfo(child, 'Due date', task.dueDate);
-    taskState(child, 'Priority', task.priority);
-    taskState(child, 'State', task.state);
+    taskState(child, 'priority', task.priority, indexTask, index);
+    taskState(child, 'state', task.state, indexTask, index);
     parent.appendChild(child);
     
 }
@@ -30,10 +31,26 @@ const taskInfo = (parent,title, detail) =>{
     child.innerHTML= `<h4>${title}: </h4> <p>${detail}</p>`;
     parent.appendChild(child);
 }
-const taskState = (parent, title, state) =>{
+const taskState = (parent, title, state, indexTask, index) =>{
     const child = document.createElement('div');
-    child.innerHTML = `<h4>${title}: </h4> <button class="${title}">${state}</button>`;
+    child.innerHTML = `<h4>${title}: </h4> <button class="${title}" data-key="${index}" data-task="${indexTask}">${state}</button>`;
+    parent.appendChild(child);    
+}
+// Button features
+const addButton = (parent, index,indexTask) =>{
+    const child = document.createElement('button');
+    child.innerHTML = 'delete';
+    child.className = 'deleteTask';
+    child.setAttribute('data-key', index);
+    child.setAttribute('data-task', indexTask);
+    child.addEventListener('click', deleteTask);
     parent.appendChild(child);
+}
+const addEventToButton = (action, name) =>{
+    const elements = document.getElementsByClassName(name);
+    [...elements].forEach((element) =>{
+        element.addEventListener('click', action);
+    });
 }
 const taskDisplay = (tasks, index) =>{
     createTaskContainer();
@@ -41,11 +58,12 @@ const taskDisplay = (tasks, index) =>{
     if(tasks.length > 0){
         parent.innerHTML = '';
         tasks.forEach((element) =>{
-        taskTitle(element.title, index);
-        taskDetail(element, index);
+        const indexTask = tasks.indexOf(element);
+        taskTitle(element.title, index, indexTask);
+        taskDetail(element, index, indexTask);
         });
+        addEventToButton(changeTaskPrior, 'priority');
+        addEventToButton(changeTaskState, 'state');
     }else parent.innerText = 'Nothing to show. Please click the button (+) to add a new task.';
-    
-
 }
 export {taskDisplay} ;
